b8dd86d3cd7b9c4589abebe7cba42b2cc410815e
RPleasnick <rpleasni@gmail.com>
2024-06-18 08:54:13 -0400

    clean up

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -82,56 +82,9 @@ d3.json(queryUrl).then(function (data) {
         console.log(div.innerHTML)
         return div;
     };
-    // legend.onAdd = function (map) {
-    //     let div = L.DomUtil.create("div", "info legend");
-    //     let depths = [0, 10, 30, 50, 70, 90];
-    //     let labels =[];
-    //     // let labels = ['#33FF66', "#FFFF33", '#FFCC66', '#FF9933', "#FF6633", "#FF3333" ];
-
-
-    //     // // Define the legend content
-    //     // let legendContent = "<h4>Depth of Earthquake</h4>" +
-    //     //     "<div class='labels'>" +
-    //     //     "<div class='label'>Label 1</div>" +
-    //     //     "<div class='label'>Label 2</div>" +
-    //     //     "<div class='label'>Label 3</div>" +
-    //     //     "</div>";
-
-    //     // Loop through depth ranges and colors
-    //     for (let i = 0; i < depths.length; i++) {
-    //         div.innerHTML += '<i style="background:' + getColor(depths[i] +1) + '"></i> ' +
-    //             depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + ' <br>' : '+ ');
-    //     }
-
-    //     console.log(div.innerHTML)
-
-    //     return div;
-
-    // };
 
     // Add the legend to the map
     legend.addTo(myMap);
 
-    // let labels = [];
-
-    // // Add the minimum and maximum.
-    // let legendInfo = "<h1>Population with Children<br />(ages 6-17)</h1>" +
-    //     "<div class=\"labels\">" +
-    //     "<div class=\"min\">" + limits[0] + "</div>" +
-    //     "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-    //     "</div>";
-
-    // div.innerHTML = legendInfo;
-
-    // limits.forEach(function (limit, index) {
-    //     labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-    // });
-
-    // div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-    // return div;
-
-
-    // Adding the legend to the map
-    // legend.addTo(myMap);
 
 });
